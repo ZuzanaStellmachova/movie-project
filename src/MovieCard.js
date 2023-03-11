9cc5620023e46a55d7aa4c24004013b3e5334326
Zuzana <113286338+ZuzanaStellmachova@users.noreply.github.com>
2023-03-11 13:02:52 +0100

    Initial commit

---
+++ src/MovieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MovieCard() {
+    return(
+        <div>
+            <div className="movie-poster"><img src="https://m.media-amazon.com/images/I/81iew7GtO9L._AC_UF894,1000_QL80_.jpg"></img></div>
+            <div className="movie-description">
+                <div className="genre">Comedy</div>
+            </div>
+        </div>
+    )
+}
